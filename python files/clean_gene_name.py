019f3ee50ca71bb4de5fc299c32aeb6443dd72f2
atanu <atanurkm11@gmail.com>
2025-06-06 13:35:47 -0600

    normalize gene names

--- python files/clean_gene_name.py
+++ python files/clean_gene_name.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-# Function to clean gene names
 def clean_gene_name(name):
     if pd.isnull(name):
         return None
-    return str(name).replace(' ', '')
+    return str(name).replace(' ', '').replace('-', '').upper()
